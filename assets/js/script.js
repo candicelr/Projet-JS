8d7d7dec838152a161d6703523b3a0612fb98701
gthiroux <thiroux.garance@gmail.com>
2024-12-10 10:50:01 +0100

    JS burger favoris

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,43 @@
-console.log("hello")
+console.log("hello");
 //  clé API
 const API_KEY = "ddcf0bf3717e635e5e6832d2cab2fcdf";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w200";
 // Toute la doc de l'API est ici : https://developer.themoviedb.org/docs/getting-started
 
-// Fonction pour afficher les films dans la liste
-const disney = ["Vaiana 2","Miraculous World : Londre, la course contre le temps","Vice-Versa 2","Mufasa : Le Roi Lion","Un conte de Noël, ou presque"]
-const classique = ["Le Grinch","Transformers : Le Commencement","Garfield : Héros malgrès lui"]
-const jeunesse = ["Le Robot sauvage","Vaiana 2","Kung Fu Panda 4","Flow, le chat qui n'avait plus peur de l'eau","Ellian et le sortilège"]
-const familial = ["Tous en scène : Thriller","Ce Noël-là","Moi, moche et méchant 4"]
-const anime = ["Watchmen: Chapter II","Solo Leveling -ReAwakening-","Overlord : The Sacred Kingdom","Le Seigneur des Anneaux : La Guerre des Rohirrim","My Hero Academia: You’re Next"]
+// liste pour trier les films dans les différentes catégories
+const disney = [
+  "Vaiana 2",
+  "Miraculous World : Londre, la course contre le temps",
+  "Vice-Versa 2",
+  "Mufasa : Le Roi Lion",
+  "Un conte de Noël, ou presque",
+];
+const classique = [
+  "Le Grinch",
+  "Transformers : Le Commencement",
+  "Garfield : Héros malgrès lui",
+];
+const jeunesse = [
+  "Le Robot sauvage",
+  "Vaiana 2",
+  "Kung Fu Panda 4",
+  "Flow, le chat qui n'avait plus peur de l'eau",
+  "Ellian et le sortilège",
+];
+const familial = [
+  "Tous en scène : Thriller",
+  "Ce Noël-là",
+  "Moi, moche et méchant 4",
+];
+const anime = [
+  "Watchmen: Chapter II",
+  "Solo Leveling -ReAwakening-",
+  "Overlord : The Sacred Kingdom",
+  "Le Seigneur des Anneaux : La Guerre des Rohirrim",
+  "My Hero Academia: You’re Next",
+];
+// fonction pour afficher les différents films.
 const displayMovies = (movies) => {
   const movieList = document.getElementById("movieList");
   movieList.innerHTML = ""; // Nettoyer la liste avant d'ajouter les films
@@ -18,15 +45,16 @@ const displayMovies = (movies) => {
   movies.forEach((movie) => {
     const div = document.createElement("div");
     div.innerHTML = `
-          <img src="${IMAGE_BASE_URL + movie.poster_path}" alt="Affiche de ${
+          <a><img src="${IMAGE_BASE_URL + movie.poster_path}" alt="Affiche de ${
       movie.title
-    }">
+    }"></a>
+        <img onclick="likes()" src="assets/img/coeur.svg" alt="pictogramme coeur">
         `;
     movieList.appendChild(div);
   });
 };
 
-// Fonction pour récupérer les films récents
+// Fonction pour récupérer les films d'animations(genre=16)
 async function fetchRecentMovies() {
   // le try catch pour éviter le plantage du script si le fetch échoue
   try {
@@ -40,10 +68,6 @@ async function fetchRecentMovies() {
     // Appel de la fonction qui affiche les films
     if (response.status !== 401) {
       displayMovies(data.results);
-    } else {
-      alert(
-        "Message du prof aux élèves :-)\nPensez à mettre votre clé d'API en ligne 40 du code !!!\nSinon, ça ne fonctionne pas\nInstructions ligne 38 dans le commentaire."
-      );
     }
   } catch (error) {
     console.error("Erreur lors de la récupération des films :", error);
@@ -52,3 +76,17 @@ async function fetchRecentMovies() {
 
 // Charger les films récents au chargement de la page
 fetchRecentMovies();
+
+// Pour aller sur la page films favoris en  cliquant sur "Voir Plus"
+const wishlist = (wishlistWindow) => {
+  wishlistWindow = window.open("witchlist.html");
+};
+// Fonction pour le menu burger du favori sur la page d'acceuil
+const menuWishlist = () => {
+  let menuBox = document.getElementById("menuBox");
+  if (menuBox.style.display == "block") {
+    menuBox.style.display = "block";
+  } else {
+    menuBox.style.display = "none";
+  }
+};
